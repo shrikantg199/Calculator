4609069c1f7655768f78fa2ad15ca31da41d7033
shrikant gaikwad <shrikantg199@gmail.com>
2023-12-20 15:26:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Calculator from "./components/Calculator";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="heading">CALCULATOR📱</h1>
+      <div className="container">
+        <Calculator />
+      </div>
+    </>
+  );
+};
+
+export default App;
